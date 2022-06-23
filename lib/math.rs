a7ea81f2e24872dd0dff659a349a679dcf013551
pSN0W <08yaksh08@gmail.com>
2022-06-23 09:34:35 +0530

    Made changes

--- lib/math.rs
+++
@@ -1,9 +0,0 @@
-pub fn add(lhs: isize, rhs: isize) -> isize {
-    lhs + rhs
-}
-pub fn sub(lhs: isize, rhs: isize) -> isize {
-    lhs - rhs
-}
-pub fn mul(lhs: isize, rhs: isize) -> isize {
-    lhs * rhs
-}
